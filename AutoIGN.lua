07e31720d333b30a1c9972dc3ac8deb674ff210b
gamsteron <gamsteron@gmail.com>
2016-06-21 09:41:37 +0200

    0.02

--- AutoIGN.lua
+++ AutoIGN.lua
@@ -11,7 +11,7 @@ if ignite == nil then
         return
 end
 
-local ver = "0.01"
+local ver = "0.02"
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/GameOnSteroids/master/AutoIGN.lua", SCRIPT_PATH .. "AutoIGN.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
@@ -23,7 +23,8 @@ GetWebResultAsync("https://raw.githubusercontent.com/gamsteron/GameOnSteroids/ma
 
 GSOAI = MenuConfig("gsoai", "GamSterOn Auto Ignite")
 GSOAI:Slider("HEALTH", "Minimum Enemy HP + Player Lvl * 5", 50,25,200,25)
-GSOAI:Boolean("SHIELD", "Calculate shields", false)
+GSOAI:Boolean("BARRIER", "Calculate Barrier", true)
+GSOAI:Boolean("SHIELD", "Calculate other shields", false)
 
 OnLoad(function()
         for i,o in pairs(GetEnemyHeroes()) do
@@ -33,16 +34,23 @@ end)
 
 OnTick(function (myHero)
         local level = GetLevel(myHero)
-        local health = GSOAI.HEALTH:Value() + ( level * 5 )
+        local dmg = 50 + (20 * level)
+        local minhp = GSOAI.HEALTH:Value() + ( level * 5 )
+        local barrier = GSOAI.BARRIER:Value()
+        local shield = GSOAI.SHIELD:Value()
         if ignite ~= nil then
                 if Ready(ignite) then
                         for i, enemy in pairs(GetEnemyHeroes()) do
                                 if ValidTarget(enemy, 600) and GSOAI[GetObjectName(enemy)]:Value() then
-                                        local hp = GetCurrentHP(enemy) + ( GetHPRegen(enemy) * 5 * 0.6 )
-                                        if GSOAI.SHIELD:Value() then
+                                        local currenthp = GetCurrentHP(enemy)
+                                        local regenhp = GetHPRegen(enemy)
+                                        local hp = currenthp + regenhp + ( regenhp * 5 * 0.6 )
+                                        if barrier and GotBuff(enemy, "SummonerBarrier") ~= 0 then
+                                                hp = hp + GetDmgShield(enemy)
+                                        elseif shield then
                                                 hp = hp + GetDmgShield(enemy)
                                         end
-                                        if hp > health and hp <= 50 + (20 * level) then
+                                        if hp > minhp and hp <= dmg then
                                                 CastTargetSpell(enemy, ignite)
                                         end
                                 end
