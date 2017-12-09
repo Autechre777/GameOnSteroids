09a9e38a7a214fd269c09b85ac772bbf91f8ad10
gamsteron <gamsteron@gmail.com>
2017-12-09 14:38:21 +0100

    Create KogMaw.lua

---
+++ KogMaw.lua
@@ -0,0 +1,73 @@
+if myHero.charName ~= "KogMaw" then return end
+
+require "OpenPredict"
+
+local spelltime = 0
+local aawind = 250
+local lastq = 0
+local lastw = 0
+local laste = 0
+local lastr = 0
+local Q = { range = 1175, speed = 1700, width = 70, delay = 0.25 }
+local E = { range = 1280, speed = 1350, width = 110, delay = 0.25 }
+local R = { range = 0, speed = math.huge, width = 100, delay = 0.6 }
+
+menu = MenuConfig("GSO", "GamSterOn KogMaw")
+    menu:KeyBinding("combo", "Combo", 32)
+
+OnProcessSpellAttack(function(unit,aa)
+        if unit.isMe then
+                aawind = aa.windUpTime * 1000
+                spelltime = GetTickCount() + aawind + 60
+        end
+end)
+
+OnIssueOrder(function(Order)
+        if Order.flag == 3 then spelltime = GetTickCount() + aawind + 120 end
+end)
+
+function WP_GetTarget(range)
+        local t = nil
+        num = 10000
+        for i, enemy in pairs(GetEnemyHeroes()) do
+                if ValidTarget(enemy, range + 100) then
+                        local mr = GetMagicResist(enemy)
+                        local hp = enemy.health * (mr/(mr+100))
+                        if hp - ((GetBonusDmg(enemy)+GetBonusAP(enemy))*2) < num then
+                                num = hp
+                                t = enemy
+                        end
+                end
+        end
+        return t
+end
+
+function WP_CastSpell(spell, spellT, col)
+        if not IsReady(spell) or GetTickCount() < spelltime or (spell == _R and GetCurrentMana(myHero) - 200 < GotBuff(myHero, "kogmawlivingartillerycost") * 40) or GetTickCount() < lastq + 250 or GetTickCount() < lastw + 250 or GetTickCount() < laste + 250 or GetTickCount() < lastr + 250 then return false end
+        if spell == _R then R.range = 900 + ( 300 * GetCastLevel(myHero, _R) ) end
+        local t = WP_GetTarget(spellT.range)
+        if t == nil then return false end
+        local pI = GetPrediction(t, spellT)
+        if pI and pI.hitChance >= 0.25 and math.sqrt( (pI.castPos.x-myHero.x)^2 + (pI.castPos.z-myHero.z)^2) < spellT.range and (col == false or not pI:mCollision(1)) then
+                CastSkillShot(spell, pI.castPos)
+                return true
+        end
+        return false
+end
+
+OnTick(function(myHero)
+        if menu.combo:Value() then
+                if WP_CastSpell(_E, E, false) == true then laste = GetTickCount() end
+                if WP_CastSpell(_Q, Q, true) == true then lastq = GetTickCount() end
+                if WP_CastSpell(_R, R, false) == true then lastr = GetTickCount() end
+                if IsReady(_W) and GetTickCount() > spelltime and GetTickCount() > lastw + 500 then
+                        for i, enemy in pairs(GetEnemyHeroes()) do
+                                if ValidTarget(enemy, 610 + (20 * GetCastLevel(myHero, _W)) + myHero.boundingRadius + enemy.boundingRadius - 30) then
+                                        CastSpell(_W)
+                                        lastw = GetTickCount()
+                                        break
+                                end
+                        end
+                end
+        end
+end)
